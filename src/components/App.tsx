d46ee4c7088e948500847ed376f0e7eeff6962e8
dbrosio3 <daniel.brosio@rootstrap.com>
2022-04-25 18:56:19 +0200

    cleanup + init template

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+
+const App = () => (
+  <BrowserRouter>
+    <Helmet>
+      <title>Black Market</title>
+    </Helmet>
+    <AppRoutes />
+  </BrowserRouter>
+);
+
+export default App;
---
+++ src/components/routes/AppRoutes.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import Landing from "./Landing";
+import Login from "./Login";
+
+const AppRoutes = () => {
+  const routes = [{ path: "/", element: <Landing /> }, { path: "/login", element: <Login /> }, ];
+  const element = useRoutes(routes);
+  return <>{element}</>;
+};
+
+export default AppRoutes;
