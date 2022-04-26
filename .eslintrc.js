12c40550754caa96a15825631d416a263927c6fe
dbrosio3 <daniel.brosio@rootstrap.com>
2022-04-26 18:41:24 +0200

    review eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,7 +44,6 @@ module.exports = {
         ],
         'linebreak-style': ['error', 'unix'],
         'react/prop-types': 'off',
-
         'import/order': [
           'error',
           {
@@ -53,11 +52,7 @@ module.exports = {
             alphabetize: { order: 'asc', caseInsensitive: true },
           },
         ],
-        'import/default': 'off',
         'import/no-named-as-default-member': 'off',
-        'import/no-named-as-default': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'jsx-a11y/anchor-is-valid': 'off',
         '@typescript-eslint/no-unused-vars': ['error'],
         '@typescript-eslint/explicit-function-return-type': ['off'],
         '@typescript-eslint/explicit-module-boundary-types': ['off'],
