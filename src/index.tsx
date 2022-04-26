cc245f64a8836dcda1dc9d186bf72d0345719da9
dbrosio3 <daniel.brosio@rootstrap.com>
2022-04-26 19:24:16 +0200

    refactor providers to simplify testing in the future

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './components/App';
+import { AppProvider } from './providers/AppProvider';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
 
