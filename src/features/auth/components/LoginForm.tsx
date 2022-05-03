5dfcc1b360facf5a836ade3a9e322eaca8d93c3f
dbrosio3 <daniel.brosio@rootstrap.com>
2022-05-03 17:47:50 +0200

    add login data state

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,39 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { chakra, Input, Text, VStack } from '@chakra-ui/react';
+import { chakra, FormControl, FormLabel, Input } from '@chakra-ui/react';
 
 import { FullWidthButton } from '@styles/theme/components/Buttons';
 import { BlackMarketLogo } from '@styles/theme/components/Logo';
 
 import { AuthSection } from './AuthSection';
 
-const InputGroup = chakra(VStack, {
+const CredentialsForm = chakra(FormControl, {
   baseStyle: {
     width: '100%',
-    marginTop: '3rem',
+    marginTop: '2.75rem',
     alignItems: 'baseline',
+    input: {
+      marginBottom: '0.75rem',
+    },
   },
 });
 
 const BottomSection = chakra('div', {
   baseStyle: {
     width: '100%',
-    marginTop: '3rem',
+    marginTop: '2.75rem',
   },
 });
 
+const fields = [
+  {
+    key: 'username',
+    label: 'Email or telephone number',
+    type: 'text',
+    placeholder: 'Type your email or telephone',
+  },
+  { key: 'password', label: 'Password', type: 'password', placeholder: 'Type your password' },
+];
+
+interface CredentialsFormState {
+  username: string;
+  password: string;
+}
+
 export const LoginForm = () => {
+  const [credentials, setCredentials] = useState<CredentialsFormState>({
+    username: '',
+    password: '',
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+
   return (
     <>
       <AuthSection height="29rem">
         <BlackMarketLogo width={174} height={31} />
-        <InputGroup>
-          <Text>Email or telephone number</Text>
-          <Input placeholder="Type your email or telephone" />
-          <Text>Password</Text>
-          <Input placeholder="Type your password" />
+        <CredentialsForm>
+          {fields.map(({ key, label, ...inputProps }) => (
+            <>
+              <FormLabel htmlFor={key}>{label}</FormLabel>
+              <Input id={key} onChange={handleInputChange} {...inputProps} />
+            </>
+          ))}
           <FullWidthButton colorScheme="secondary">Log in</FullWidthButton>
-        </InputGroup>
+        </CredentialsForm>
         <BottomSection>
           <FullWidthButton colorScheme="lightblue" variant="link">
             I forgot my password
