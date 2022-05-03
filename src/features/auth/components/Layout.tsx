17b2241f246155e5f24f62dc25c934f06673ad99
dbrosio3 <daniel.brosio@rootstrap.com>
2022-05-03 11:01:07 +0200

    fixed background

--- src/features/auth/components/Layout.tsx
+++ src/features/auth/components/Layout.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import { Box } from '@chakra-ui/react';
+
+import chairsBg from '@/assets/chairs-bg.png';
 import { JustChildrenProp } from '@/types';
 
 type Props = JustChildrenProp;
 
 export const Layout = (props: Props) => {
-  return <div>Layout</div>;
+  return (
+    <Box
+      w="100vw"
+      h="100vh"
+      bg="secondary.900"
+      bgImg={chairsBg}
+      backgroundSize="cover"
+      overflow="hidden"
+    />
+  );
 };
