521210d6ba70ac27f503a56d442cdf1f302ae44f
dbrosio3 <daniel.brosio@rootstrap.com>
2022-05-11 14:33:23 +0200

    improve tests on helper fn

--- src/__mocks__/reacti18nextMock.ts
+++ src/__mocks__/reacti18nextMock.ts
@@ -3,7 +3,7 @@ import { TFunction } from 'i18next';
 const reactI18Next: { t: TFunction } = jest.createMockFromModule('react-i18next');
 
 reactI18Next.t = (key, options) => ({
-  newKey: key,
+  key,
   options,
 });
 
