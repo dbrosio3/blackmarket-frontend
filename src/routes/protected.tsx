29ce6a9b70604831e60b486384a404ccbae70f94
dbrosio3 <daniel.brosio@rootstrap.com>
2022-05-17 14:38:07 +0200

    add session data to dashboard

--- src/routes/protected.tsx
+++ src/routes/protected.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 import { Navigate, Outlet } from 'react-router-dom';
 
+import { AppLayout } from '@components/Layout/AppLayout';
 import { Dashboard, MockRoute } from '@features/misc';
 
 import { PATHS } from './paths';
 
 const App = () => {
   return (
-    <>
-      App Layout
+    <AppLayout>
       <Outlet />
-    </>
+    </AppLayout>
   );
 };
 
